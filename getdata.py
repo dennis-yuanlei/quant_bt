ac05e6ba997c692a331ef4548a5174319c504b1b
yuanlei <lei_y@hotmail.com>
2023-10-08 23:42:08 +0800

    update

--- getdata.py
+++ getdata.py
@@ -2,7 +2,7 @@ import akshare as ak
 import ipdb
 import pandas
 
-stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="601127", period="daily", start_date="20230101", end_date='202301231', adjust="qfq")
+stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="601127", period="daily", start_date="20210101", end_date='202301231', adjust="hfq")
 stock_zh_a_hist_df.to_csv('601127.csv', index=False)
 # ipdb.set_trace()
 print(stock_zh_a_hist_df)
--- macd_up.py
+++ macd_up.py
@@ -29,114 +29,18 @@ class ACommission(bt.CommInfoBase):
             else:
                 return 0
 
-# Create a Stratey
-class TestStrategy(bt.Strategy):
-    params = (
-        ('maperiod', 15),
-    )
-
-    def log(self, txt, dt=None):
-        ''' Logging function fot this strategy'''
-        dt = dt or self.datas[0].datetime.date(0)
-        print('%s, %s' % (dt.isoformat(), txt))
+# create a signal
+class MacdUpSignal(bt.Indicator):
+    lines = ('signal',)
+    params = (('period', 15),)
 
     def __init__(self):
-        # Keep a reference to the "close" line in the data[0] dataseries
-        self.dataclose = self.datas[0].close
-
-        # To keep track of pending orders and buy price/commission
-        self.order = None
-        self.buyprice = None
-        self.buycomm = None
-
-        # Add a MovingAverageSimple indicator
-        self.sma = bt.indicators.SimpleMovingAverage(
-            self.datas[0], period=self.params.maperiod)
-
-        # Indicators for the plotting show
-        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25, plot=False)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=False)
-        bt.indicators.StochasticSlow(self.datas[0], plot=False)
-        self.macd = bt.indicators.MACDHisto(self.datas[0], plot=True)
-        rsi = bt.indicators.RSI(self.datas[0], plot=False)
-        bt.indicators.SmoothedMovingAverage(rsi, period=10, plot=False)
-        bt.indicators.ATR(self.datas[0], plot=False)
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-            return
-
-        # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log(
-                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                    (order.executed.price,
-                     order.executed.value,
-                     order.executed.comm))
-
-                self.buyprice = order.executed.price
-                self.buycomm = order.executed.comm
-            else:  # Sell
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                         (order.executed.price,
-                          order.executed.value,
-                          order.executed.comm))
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        # Write down: no pending order
-        self.order = None
-
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
-
-    def next(self):
-        ipdb.set_trace()
-        # Simply log the closing price of the series from the reference
-        self.log('Close, %.2f' % self.dataclose[0])
-
-        # Check if an order is pending ... if yes, we cannot send a 2nd one
-        if self.order:
-            return
-
-        # Check if we are in the market
-        if not self.position:
-
-            # Not yet ... we MIGHT BUY if ...
-            if self.dataclose[0] > self.sma[0]:
-
-                # BUY, BUY, BUY!!! (with all possible default parameters)
-                self.log('BUY CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy(exectype=bt.Order.Market)
-
-        else:
-
-            if self.dataclose[0] < self.sma[0]:
-                # SELL, SELL, SELL!!! (with all possible default parameters)
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-
-                # Keep track of the created order to avoid a 2nd order
-                self.order = self.sell(exectype=bt.Order.Market)
+        self.lines.signal = self.data - bt.indicators.SMA(period=self.p.period)
 
 
 if __name__ == '__main__':
     # Create a cerebro entity
     cerebro = bt.Cerebro()
-
-    # Add a strategy
-    cerebro.addstrategy(TestStrategy)
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
@@ -160,6 +64,11 @@ if __name__ == '__main__':
 
     # Add the Data Feed to Cerebro
     cerebro.adddata(data, name='601127')
+
+    # Add a signal
+    cerebro.add_signal(bt.SIGNAL_LONG, MacdUpSignal)
+    cerebro.signal_accumulate(False)
+    cerebro.signal_concurrent(True)
 
     # Set our desired cash start
     cerebro.broker.setcash(10000.0)
